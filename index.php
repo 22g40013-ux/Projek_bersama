02b48f4991117757df69256ac1d685fc921d9a97
22n40019-lgtm <22n40019@student.unika.ac.id>
2025-11-12 12:10:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Informasi Katalog Kopi Kita</title>
     <link rel="stylesheet" href="style.css">
+ <style>
+        /* Reset dasar */
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
