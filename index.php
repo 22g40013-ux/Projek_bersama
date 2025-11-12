5adea9f477e7ed94feedefb50297bf71d11a7e1b
22n40019-lgtm <22n40019@student.unika.ac.id>
2025-11-12 12:11:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,4 +26,12 @@
             background: url('images/kopi.jpeg') no-repeat center center/cover;
             position: relative;
         }
+.hero::after {
+            content: "";
+            position: absolute;
+            top: 0; left: 0;
+            width: 100%; height: 100%;
+            background: rgba(0,0,0,0.6);
+            z-index: 0;
+        }
 
